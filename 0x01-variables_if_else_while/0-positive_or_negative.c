236911887f6e935d768b69801dea175d485c4dbd
Precious Chukwuezi <blessedtobe30@gmail.com>
2022-12-12 17:04:37 -0800

    Task 0,stay positive, or neutral not zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point
  *
  * Return : Always 0 (Success)
  */
+
 int main(void){
 	int n;
 
@@ -13,11 +15,11 @@ int main(void){
 
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("is positive\n");
+		printf("%d is positive\n", n);
 	if (n == 0)
-		printf("is zero\n");
+		printf("%d is zero\n", n);
 	if (n < 0)
-		printf("is negative\n");
+		printf("%d is negative\n", n);
 
 	return (0);
 }
