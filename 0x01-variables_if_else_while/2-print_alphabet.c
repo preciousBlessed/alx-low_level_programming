f65878cdac976ab17e3f06104aef3cb910f91630
Precious Chukwuezi <blessedtobe30@gmail.com>
2022-12-23 00:09:06 -0800

    Play the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
