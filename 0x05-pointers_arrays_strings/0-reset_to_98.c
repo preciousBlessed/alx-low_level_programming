3b68a79b4977d74ea37f28f9fd651ac5f9bf324d
Precious Chukwuezi <blessedtobe30@gmail.com>
2023-01-03 17:27:50 -0800

    Reset n to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * reset_to_98 - this function modifies a value n
+ * to 98
+ * @n:a pointer to an integer to modufy
+ *
+ * Return: nothing.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,7 @@
+# 0x05-pointers_arrays_strings
+
+## Task 0
+* Reset n to 98 with pointer logic
+
+## Task 1
+* 
